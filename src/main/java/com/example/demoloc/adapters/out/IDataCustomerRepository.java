3fe6328fa6a0cb77b225866ab481c06e6551a8f7
alinenosekai <141348800+alinenosekai@users.noreply.github.com>
2025-08-19 08:44:43 +0200

    Update IDataCustomerRepository.java

--- src/main/java/com/example/demoloc/adapters/out/IDataCustomerRepository.java
+++ src/main/java/com/example/demoloc/adapters/out/IDataCustomerRepository.java
@@ -5,13 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 
 public interface IDataCustomerRepository extends JpaRepository<Customer, Integer> {
 
     @Query(value = "SELECT * FROM customer c WHERE c.firstName = :firstName AND c.lastName = :lastName", nativeQuery = true)
     List<Customer> findByFirstNameAndLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
 
-    List<Customer> findByFirstNameAndLastName(String firstName, String lastName);
     List<Customer> findByFullName(String fullName);
     List<Customer> findByEmail(String email);
 }
